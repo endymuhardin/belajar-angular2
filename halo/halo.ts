c90e790810bdaf7cfb178a4178a6b303b598a707
Endy Muhardin <endy.muhardin@gmail.com>
2015-12-27 22:52:48 +0700

    implement hello world

---
+++ halo/halo.ts
@@ -0,0 +1,16 @@
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'pengunjung',
+    template:`
+    Nama : <input type="text" #tamu (keyup)="updateNama(tamu.value)"> <br />
+    <h2>Halo {{nama}}</h2>
+    `
+})
+export class HaloComponent {
+    nama: string = "belum ada";
+    
+    updateNama(x: string){
+        this.nama = x;
+    }
+ }
