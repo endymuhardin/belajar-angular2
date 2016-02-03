851988a9f7bfe35448e398980b3ea72117c926d4
Endy Muhardin <endy.muhardin@gmail.com>
2016-02-03 13:02:37 +0700

    menambahkan twitter bootstrap

--- angular2-jspm/aplikasi/boot.ts
+++ angular2-jspm/aplikasi/boot.ts
@@ -2,5 +2,7 @@ import 'reflect-metadata';
 import {bootstrap} from "angular2/platform/browser";
 import {HaloComponent} from "./halo/halo.component";
 
+import 'bootstrap/css/bootstrap.css!';
+
 console.log("Bootstrapping Angular 2");
 bootstrap(HaloComponent);
